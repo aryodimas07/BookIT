21d59458587bb7ab90041bf57087040b5e958d6e
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2018-12-04 23:12:10 +0700

    added : adapter for movie item(still error) and item for adapter

--- app/src/main/java/com/filkom/aryodimas/bookit/MovieListFragment.java
+++ app/src/main/java/com/filkom/aryodimas/bookit/MovieListFragment.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.filkom.aryodimas.bookit.adapter.MovieItemAdapter;
 
 import java.util.List;
 
@@ -26,43 +29,9 @@ public class MovieListFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         RecyclerView rv = new RecyclerView(getContext());
-        rv.setLayoutManager(new LinearLayoutManager(getContext()));
-        rv.setAdapter(new SimpleRVAdapter(strings));
+        rv.setLayoutManager(new GridLayoutManager(getContext(),2));
+        rv.setAdapter(new MovieItemAdapter(strings));
         return rv;
     }
 
-    public class SimpleRVAdapter extends RecyclerView.Adapter<SimpleViewHolder> {
-        private String[] dataSource;
-        public SimpleRVAdapter(String[] data){
-            dataSource = data;
-        }
-
-        @NonNull
-        @Override
-        public SimpleViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-            View view = new TextView(viewGroup.getContext());
-            SimpleViewHolder viewHolder = new SimpleViewHolder(view);
-            return viewHolder;
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull SimpleViewHolder simpleViewHolder, int i) {
-            simpleViewHolder.textView.setText(dataSource[i]);
-        }
-
-
-        @Override
-        public int getItemCount() {
-            return dataSource.length;
-        }
-    }
-
-    public static class SimpleViewHolder extends RecyclerView.ViewHolder{
-        public TextView textView;
-
-        public SimpleViewHolder(@NonNull View itemView) {
-            super(itemView);
-            textView = (TextView) itemView;
-        }
-    }
 }
