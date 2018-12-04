ffce07cd31f8c6f7581c293aa0c4bd94e39479a5
aryodimas07 <aryodimas07@student.ub.ad.id>
2018-12-05 06:11:38 +0700

    add homepage, fragment movie list

--- app/src/main/java/com/filkom/aryodimas/bookit/MainActivity.java
+++ app/src/main/java/com/filkom/aryodimas/bookit/MainActivity.java
@@ -9,5 +9,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getNowPlayingFragment();
+        getHotMovieFragment();
     }
+
+    private void getNowPlayingFragment(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment1Container, new NowPlayingFragment()).commit();
+    }
+
+    private void getHotMovieFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment2Container, new HotMovieFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment3Container, new HotMovieFragment()).commit();
+    }
+
 }
---
+++ app/src/main/java/com/filkom/aryodimas/bookit/MovieViewAdapter.java
@@ -0,0 +1,48 @@
+package com.filkom.aryodimas.bookit;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class MovieViewAdapter extends RecyclerView.Adapter<MovieViewAdapter.MovieViewHolder>{
+    private Context mContext;
+    private ArrayList<Movie> movieList;
+
+    public MovieViewAdapter(Context mContext, ArrayList<Movie> orderList) {
+        this.mContext = mContext;
+        this.movieList = orderList;
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_nowplaying, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, final int position) {
+        Movie movie = movieList.get(position);
+
+        holder.ivMovie.setImageResource(movie.movPicId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView ivMovie;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+            ivMovie = itemView.findViewById(R.id.iv_movie);
+        }
+    }
+}
